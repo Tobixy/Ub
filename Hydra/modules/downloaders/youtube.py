d26eda210679007c0669ac23a7786dd807b10e06
Vishal-1756 <126957307+Vishal-1756@users.noreply.github.com>
2023-06-12 13:00:46 +0530

    Update youtube.py

--- Hydra/modules/downloaders/youtube.py
+++ Hydra/modules/downloaders/youtube.py
@@ -1,6 +1,6 @@
 from os import path
 from yt_dlp import YoutubeDL
-from AdityaHalder.modules.helpers.errors import DurationLimitError
+from Hydra.modules.helpers.errors import DurationLimitError
 
 ydl_opts = {
     "format": "bestaudio/best",
