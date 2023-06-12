9e1f052ac076570774e4904e71f31e8437badb25
Vishal-1756 <126957307+Vishal-1756@users.noreply.github.com>
2023-06-12 12:53:31 +0530

    Update writetool.py

--- Hydra/plugins/writetool.py
+++ Hydra/plugins/writetool.py
@@ -4,7 +4,7 @@ import requests
 import Hydra.config
 
 
-@Cleint.on_message(filters.user(config.OWNER_ID) & filters.command("write",prefixes=config.))
+@Cleint.on_message(filters.user(config.OWNER_ID) & filters.command("write",prefixes=config.COMMAND_PREFIXES))
 async def handwriting(_, message):
     if len(message.command) < 2:
         return await message.reply_text("» Give some text to write...")
